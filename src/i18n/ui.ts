7a7a1612d4db0a7bc075531a5e41123bf7f01b09
AlfredoNavarro0 <128437487+AlfredoNavarro0@users.noreply.github.com>
2025-03-30 01:57:19 -0700

    Banner de Landing Page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,24 @@ export const ui = {
         "clients.title": "Nuestros clientes",
         "footer.email-alt": "Correo de contacto",
         "footer.fb-alt": "Página de Facebook",
-        "footer.ig-alt": "Página de Instagram"
+        "footer.ig-alt": "Página de Instagram",
+        "banner.title": "EVOGROWTH",
+        "banner.DescOne": "DESARROLLO WEB A TU MEDIDA",
+        "banner.DescTwo": "LOREM IPSUM LOREM IPSUM LOREM",
+        "banner.DescThree": "IMPULSA TU NEGOCIO",
+        "banner.DescFour": "LOREM IPSUM LOREM IPSUM LOREM",
+        "banner.hint": "CONOCE MAS",
     },
     en: {
         "clients.title": "Our clients",
         "footer.email-alt": "Contact email",
         "footer.fb-alt": "Facebook page",
-        "footer.ig-alt": "Instagram page"
+        "footer.ig-alt": "Instagram page",
+        "banner.title": "EVOGROWTH",
+        "banner.DescOne": "CUSTOM WEB DEVELOPMENT",
+        "banner.DescTwo": "LOREM IPSUM LOREM",
+        "banner.DescThree": "BOOST YOUR BUSINESS",
+        "banner.DescFour": "LOREM IPSUM LOREM IPSUM LOREM",
+        "banner.hint": "LEARN MORE",
     },
 } as const;
