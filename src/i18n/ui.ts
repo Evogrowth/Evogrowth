75399cc169afa65fbcaf1bd9837e70e5da0e5249
Carlos Garcia <carlosgarcia.dev@outlook.com>
2025-03-29 23:17:36 -0700

    feat: add navbar to the landing page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,12 +7,20 @@ export const defaultLang = 'es';
 
 export const ui = {
     es: {
+        "navbar.menu": "Menú",
+        "navbar.home": "Inicio",
+        "navbar.portfolio": "Portafolio",
+        "navbar.contact": "Contacto",
         "clients.title": "Nuestros clientes",
         "footer.email-alt": "Correo de contacto",
         "footer.fb-alt": "Página de Facebook",
         "footer.ig-alt": "Página de Instagram"
     },
     en: {
+        "navbar.home": "Home",
+        "navbar.menu": "Menu",
+        "navbar.portfolio": "Portfolio",
+        "navbar.contact": "Contact",
         "clients.title": "Our clients",
         "footer.email-alt": "Contact email",
         "footer.fb-alt": "Facebook page",
