5ec6c91af88ca0b45c4059aa20feca62195c31c0
Julian Lopez <a20490712@itmexicali.edu.mx>
2025-04-01 18:20:48 -0700

    feat: Agrega cambio de idioma y fixes sugeridos por Carlos

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,30 @@ export const ui = {
         "clients.title": "Nuestros clientes",
         "footer.email-alt": "Correo de contacto",
         "footer.fb-alt": "Página de Facebook",
-        "footer.ig-alt": "Página de Instagram"
+        "footer.ig-alt": "Página de Instagram",
+        "contact.title": "Lleva tu negocio al siguiente nivel",
+        "contact.subtitle": "Software que se ajusta a tus necesidades",
+        "contact.form-title": "¿Listo para iniciar tu próximo proyecto?",
+        "contact.form-input-name": "Nombre completo",
+        "contact.form-input-email": "Correo electrónico",
+        "contact.form-input-phone": "Teléfono",
+        "contact.form-input-description": "Descripción breve del proyecto",
+        "contact.form-button": "Solicitar cotización",
+        "evogrowth.logo-alt": "Evogrowth - Desarrollo de Software"
     },
     en: {
         "clients.title": "Our clients",
         "footer.email-alt": "Contact email",
         "footer.fb-alt": "Facebook page",
-        "footer.ig-alt": "Instagram page"
+        "footer.ig-alt": "Instagram page",
+        "contact.title": "Take your business to the next level",
+        "contact.subtitle": "Software tailored to your needs",
+        "contact.form-title": "Ready to start your next project?",
+        "contact.form-input-name": "Full name",
+        "contact.form-input-email": "E-mail address",
+        "contact.form-input-phone": "Phone number",
+        "contact.form-input-description": "Short description of your project",
+        "contact.form-button": "Request quote",
+        "evogrowth.logo-alt": "Evogrowth - Software Development"
     },
 } as const;
