59f472d67faf4266051b9132a63e730ed17cd539
Isaac Fernandez <i.fernandezdo20@gmail.com>
2025-04-02 23:41:49 -0700

    Added form validations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [alpinejs({ entrypoint: '/src/entrypoint' })],
   vite: {
     plugins: [tailwindcss()]
   },
